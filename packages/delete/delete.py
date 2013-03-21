6841d5e8fb21e20c03a7a8e2c1186326c1d2c892
rumkin <hello@rumk.in>
2013-03-21 11:17:40 +0400

    Delete image code moved to package delete

--- packages/delete/delete.py
+++ packages/delete/delete.py
@@ -1,3 +1,26 @@
+# /home/YOURUSERFOLDER/.local/share/Trash/files/
+
+from time import strftime, localtime
+import shutil
+trashDir = os.getenv("HOME") + "/.local/share/Trash"
+
+def moveToTrash(filename):
+  if not os.path.exists(filename):
+    return
+
+  if not os.path.exists(trashDir):
+    return
+
+  trashInfo  = trashDir + "/info/" + os.path.basename(filename) + ".trashinfo"
+  trashFiles = trashDir + "/files"
+
+  with open(trashInfo, "w") as info:
+    content = '[Trash Info]\r\nPath=' + filename +'\r\nDeletetionDate=' + strftime("%Y-%m-%dT%H:%M:%S", localtime()) + '\r\n'
+    info.write(content)
+  info.closed
+
+  shutil.move(filename, trashFiles)
+
 
 class delete:
 	def __init__(self, iv, config):
